d6e1ddf103b3b6b97949dfce20c78be509418a3f
MAC <mac@MACs-MacBook-Pro.local>
2020-04-05 12:17:24 +0530

    initial commit

---
+++ DApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DApp.API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    private readonly DataContext _context;
+
+    public ValuesController(DataContext context)
+    {
+      _context = context;
+
+    }
+    //GET api/values
+    [HttpGet]
+    public async Task<IActionResult> GetValues()
+    {
+      //throw new Exception("Test Exception");
+      var values = await _context.Values.ToListAsync();
+      return Ok(values);
+      //return new string[] { "value1", "value3" };
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetValue(int id)
+    {
+      //throw new Exception("Test Exception");
+      var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+      return Ok(value);
+      //return new string[] { "value1", "value3" };
+    }
+
+  }
+}
